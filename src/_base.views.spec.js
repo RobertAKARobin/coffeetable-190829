c6fb54675cb3661c14dd07a6b7fd400e5fa4305e
RobertAKARobin <robertgfthomas@gmail.com>
2018-12-24 09:58:29 -0600

    Added view specs

--- src/_base.views.spec.js
+++ src/_base.views.spec.js
@@ -1,11 +1,34 @@
 function frame(){
 	return new Promise (requestAnimationFrame)
 }
+const componentToDOMMapping = {
+	'tables': 'table',
+	'rows': 'tr',
+	'cells': 'td'
+}
+function DOM(){
+	let root, selector
+	if(arguments.length > 1){
+		[root, selector] = arguments
+	}else{
+		[selector] = arguments
+	}
+	return Array.from((root || document).querySelectorAll(componentToDOMMapping[selector]))
+}
 o.spec('In browser', ()=>{
+	const data = {}
 	o.beforeEach(()=>{
+		state.table = Table.create(Data)
+		
+		data.table = JSON.parse(JSON.stringify(Data))
+		data.rows = data.table.rows
+		data.cells = data.rows.map(r=>r.cells).flat()
+
 		m.mount(document.getElementById('app-output'), Table.component)
 	})
-	o('renders', ()=>{
-		o(document.querySelectorAll('tr').length).equals(Data.rows.length)
+	o('on load', ()=>{
+		o(DOM('tables').length).equals(1)
+		o(DOM('rows').length).equals(data.rows.length)
+		o(DOM('cells').map(c=>c.textContent)).deepEquals(data.cells.map(c=>c.datum))
 	})
 })
