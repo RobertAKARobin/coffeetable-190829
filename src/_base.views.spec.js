b8ac42c088ee6667bb5130c8fa87ccc43fb9189b
RobertAKARobin <robertgfthomas@gmail.com>
2018-12-26 13:03:48 -0600

    Added spec for creating/removing

--- src/_base.views.spec.js
+++ src/_base.views.spec.js
@@ -4,7 +4,15 @@ function frame(){
 const componentToDOMMapping = {
 	'tables': 'table',
 	'rows': 'tbody.body tr',
-	'cells': 'td'
+	'cells': 'td',
+	'columns': 'col',
+
+	'createRow': '[action=createRow]',
+	'removeRow': '[action=removeRow]',
+
+	'createColumn': '[action=createColumn]',
+	'removeColumn': '[action=removeColumn]'
+
 }
 function DOM(){
 	let root, selector
@@ -29,6 +37,27 @@ o.spec('In browser', ()=>{
 	o('on load', ()=>{
 		o(DOM('tables').length).equals(1)
 		o(DOM('rows').length).equals(input.rows.length)
+		o(DOM('columns').length).equals(Math.max(...input.rows.map(r=>r.cells.length)))
 		o(DOM('cells').map(c=>c.textContent)).deepEquals(input.cells.map(c=>c.data))
+	})
+	o('on click createRow', ()=>{
+		const initialNumberOfRows = DOM('rows').length
+		DOM('createRow')[0].click()
+		o(DOM('rows').length).equals(initialNumberOfRows + 1)
+	})
+	o('on click removeRow', ()=>{
+		const initialNumberOfRows = DOM('rows').length
+		DOM('removeRow')[0].click()
+		o(DOM('rows').length).equals(initialNumberOfRows - 1)
+	})
+	o('on click createColumn', ()=>{
+		const initialNumberOfColumns = DOM('columns').length
+		DOM('createColumn')[0].click()
+		o(DOM('columns').length).equals(initialNumberOfColumns + 1)
+	})
+	o('on click removeColumn', ()=>{
+		const initialNumberOfColumns = DOM('columns').length
+		DOM('removeColumn')[0].click()
+		o(DOM('columns').length).equals(initialNumberOfColumns - 1)
 	})
 })
--- src/table.views.js
+++ src/table.views.js
@@ -2,18 +2,47 @@ Table.component = {
 	view: function(vnode){
 		const table = vnode.attrs.table
 		return m('table', [
+			m('colgroup', [
+				table.getWidth().map(index=>{
+					return m('col')
+				})
+			]),
 			m('tbody.head', [
 				m('tr', [
 					m('th'),
 					table.getWidth().map(index=>{
-						return m('th', index)
+						return m('th', [
+							index,
+							m('button[action=createColumn]', {
+								onclick(event){
+									table.createColumn([]).placeAt(index)
+								}
+							}, '-'),
+							m('button[action=removeColumn]', {
+								onclick(event){
+									table.removeColumnAt(index)
+								}
+							}, '+')
+						])
 					})
 				])
 			]),
 			m('tbody.body', [
 				table.getChildren().map((row, index)=>{
 					return m('tr', [
-						m('th', index),
+						m('th', [
+							index,
+							m('button[action=createRow]', {
+								onclick(event){
+									table.createChild({}).placeAt(index)
+								}
+							}, '-'),
+							m('button[action=removeRow]', {
+								onclick(event){
+									table.removeChildAt(index)
+								}
+							}, '+')
+						]),
 						m(Row.component, {row})
 					])
 				})
