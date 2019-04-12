268a5c65b8249a452023c4ce1b51db252baa3886
RobertAKARobin <robertgfthomas@gmail.com>
2019-04-12 15:36:14 -0500

    Dont really need record views

--- src/collection.views.js
+++ src/collection.views.js
@@ -2,9 +2,15 @@ Collection.component = {
 	view: function(vnode){
 		const collection = vnode.attrs.collection
 		return m('table[collection]', [
-			m('thead', collection.getColumnNames().map(columnName=>m('th', columnName))),
+			m('thead', [
+				m('tr', [
+					collection.getColumnNames().map(columnName=>m('th', columnName))
+				])
+			]),
 			m('tbody.body', [
-				collection.getRecords().map(record=>m(Record.component, {record}))
+				collection.getRecords().map(record=>m('tr[record]', [
+					record.getColumns().map(value=>m('td', `${value}`))
+				]))
 			])
 		])
 	}
--- src/record.views.js
+++
@@ -1,8 +0,0 @@
-Record.component = {
-	view: function(vnode){
-		const record = vnode.attrs.record
-		return m('tr[record]', [
-			record.getColumns().map(value=>m('td', `${value}`))
-		])
-	}
-}
